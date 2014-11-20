47e52f83dd8b59934b119cb906154c4ac5b3c0c9
msyinmei <yin.codes@gmail.com>
2014-11-19 17:00:06 -0800

    removed extra views and edited config initializers omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'process.env.SKETCHPHRASE_FBID', 'process.env.SKETCHPHRASE_FBSECRET'
+  provider :facebook, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET
 end
