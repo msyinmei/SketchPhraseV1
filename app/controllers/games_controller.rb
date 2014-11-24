46cbddf2bbde75dc8dd4ae663a966aa2ef4f33ba
msyinmei <yin.codes@gmail.com>
2014-11-24 14:27:07 -0800

    send players count to games index to access via clownhats in ruby\. if needed\, can revise the url params to display players instead of the game id \(which is what it currently displays\)

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,16 +3,21 @@ class GamesController < ApplicationController
   # before_action :confirm_logged_in
 
 	def index
+    @user = User.find(session[:user_id])
+    @games = @user.games
+    @game = @games.last
+    # @game = Game.find_all_by_user_id(session[:user_id]).last
     @players_count = @game.players_count
 	end
 
   def create
     @game = Game.new
-    if Game.save
+    if @game.save
       if session[:user_id]
         @game.user_id = session[:user_id]
+        @game.players_count = params[:players_count]
+        @game.save
       end
-      @game.players_count = params[:players_count]
       redirect_to games_path(@game.id)
     end
   end
