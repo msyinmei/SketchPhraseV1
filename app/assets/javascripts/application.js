653a19f2c73402795ab844d0c67be0b914588ecf
msyinmei <yin.codes@gmail.com>
2014-11-23 16:18:16 -0800

    increase and reduce players

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -105,8 +105,6 @@
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
         //may be able to redirect instead
     });
   }
