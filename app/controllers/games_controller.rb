03b1c60d56e52737a71e57a86accae61e93372bf
msyinmei <yin.codes@gmail.com>
2014-11-24 13:49:57 -0800

    added responsive buttons for players count and routed to create new game with players number in the database

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,20 +3,18 @@ class GamesController < ApplicationController
   # before_action :confirm_logged_in
 
 	def index
-
+    @players_count = @game.players_count
 	end
 
   def create
     @game = Game.new
-    if session[:user_id]
-      @game.user_id = session[:user_id]
+    if Game.save
+      if session[:user_id]
+        @game.user_id = session[:user_id]
+      end
+      @game.players_count = params[:players_count]
+      redirect_to games_path(@game.id)
     end
-    session[:players_count] = params[:players_count]
-    redirect_to game_path(@game)
-  end
-
-  def show
-
   end
 
 end
