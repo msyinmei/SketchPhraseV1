36c2295b0201cede31bf39f0e6bb423804bc7738
msyinmei <yin.codes@gmail.com>
2014-11-24 16:35:32 -0800

    getting background crazieness to work on heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -119,24 +119,24 @@
 
 // CODE FOR THE DYNAMIC GRADIENT
 
-var colors = new Array(
-  [62,35,255],
-  [60,255,60],
-  [255,35,98],
-  [45,175,230],
-  [255,0,255],
-  [255,128,0]);
+// var colors = new Array(
+//   [62,35,255],
+//   [60,255,60],
+//   [255,35,98],
+//   [45,175,230],
+//   [255,0,255],
+//   [255,128,0]);
 
-var step = 0;
-//color table indices for:
-// current color left
-// next color left
-// current color right
-// next color right
-var colorIndices = [0,1,2,3];
+// var step = 0;
+// //color table indices for:
+// // current color left
+// // next color left
+// // current color right
+// // next color right
+// var colorIndices = [0,1,2,3];
 
-//transition speed
-var gradientSpeed = 0.002;
+// //transition speed
+// var gradientSpeed = 0.002;
 
 function updateGradient()
 {
